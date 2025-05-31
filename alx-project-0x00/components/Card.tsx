cf93e1a270928a1f296ff1452625a7053c209881
Cate Nduati <nduaticate001@gmail.com>
2025-05-31 07:30:46 +0300

    components

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
-import HOUSE_IMAGE from "@/public/assets/house.png"
-import STAR_IMAGE from "@/public/assets/star.png"
+import HOUSE_IMAGE from "@/public/assets/images/house.png"
+import STAR_IMAGE from "@/public/assets/images/star.png"
 import React from "react"
 import Pill from "./Pill"
 
@@ -10,7 +10,7 @@ const Card: React.FC = () => {
  return (
     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
       <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
-      <div className="p-2 flex gap-2 mt-2">
+      <div className="p-2 flex gap-2 mt-2 text-[#323131]">
         <Pill title="Top Villa" />
         <Pill title="Self CheckIn" />
         <Pill title="Free Reschedule" />
